4e93e9c2f7bbcfc685e51f19fb7e4ca5e9f5cd97
Ulvi019 <007esedzade00@gmail.com>
2023-03-30 16:06:20 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+let id=document.querySelector("#section")
+
+let img1=document.querySelector("id,.img1")
+let img2=document.querySelector("id,.img2")
+// let img3=document.querySelector(".img3")
+// let img4=document.querySelector(".img4")
+// let img5=document.querySelector(".img5")
+
+let left=document.querySelector("id,.left")
+let right=document.querySelector("id,.right")
+
+
+right.addEventListener("click",function(){
+img1.style=
+`
+opacity:0;
+`
+img2.style=
+`
+opacity:5;
+
+`    
+})
